7d0c627e4cbed6565c3d1fbeb6c7839be43e4316
JunichiQueen <junichim@hotmail.com>
2019-08-16 15:34:02 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const app = express();
+
+mongoose.connect('mongodb://localhost:27017/', { newUrlParser: true }).then(
+    () => { console.log("connection ready")},
+    (err) => { console.log(err)}
+);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`server running on port ${port}`));
